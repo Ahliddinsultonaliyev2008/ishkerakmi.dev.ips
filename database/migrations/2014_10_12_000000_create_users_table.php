9affd46b214a908b2de9b2fd4ba818d20cda94b9
AbdulazizbekCoder <abdulazizbek.coder@gmail.com>
2024-04-01 15:21:46 +0500

    database struct one version

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->integer('age');
+            $table->string('phone')->unique();
+            $table->string('image')->nullable();
+            $table->string('profession');
+            $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
